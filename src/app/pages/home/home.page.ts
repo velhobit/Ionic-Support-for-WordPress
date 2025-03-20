5e2cf04fdd3aaf91ff7fa0ce2bb2e4ed78d2014e
Rodrigo Portillo <portillo@rodrigos-macbook-pro.local>
2025-03-20 17:19:34 -0300

    Quick fix

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -145,8 +145,6 @@ export class HomePage implements OnInit {
   loadBanners() {
     this.wpService.getBanners('main-banners').subscribe((data) => {
       this.banners = data.banners;
-      console.log(data);
-      console.log(data.banners);
     });
   }
 
